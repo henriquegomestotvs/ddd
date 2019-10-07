d8803d0a619e7d7a95ada0f0530ab1253260baab
Henrique Glicério Da Conceição Gomes <henrique.conceicao@totvs.com.br>
2019-10-07 09:00:33 -0300

    Primeiro Checkin

---
+++ dotnetcore/dnc/sources/dnc.infra.app.mapper.core/AppAdapter.cs
@@ -0,0 +1,35 @@
+﻿using FastMapper.NetCore;
+using System.Collections.Generic;
+
+namespace dnc.infra.app.mapper.core
+{
+  public abstract class AppAdapter<TEntity, TViewModel>
+    where TEntity : class
+    where TViewModel : class
+  {
+    protected AppAdapter()
+    {
+
+    }
+
+    public virtual TViewModel To(TEntity entity)
+    {
+      return TypeAdapter.Adapt<TEntity, TViewModel>(entity);
+    }
+
+    public virtual IEnumerable<TViewModel> To(IEnumerable<TEntity> entities)
+    {
+      return TypeAdapter.Adapt<IEnumerable<TEntity>, IEnumerable<TViewModel>>(entities);
+    }
+
+    public virtual TEntity To(TViewModel viewModel)
+    {
+      return TypeAdapter.Adapt<TViewModel, TEntity>(viewModel);
+    }
+
+    public virtual IEnumerable<TEntity> To(IEnumerable<TViewModel> viewModels)
+    {
+      return TypeAdapter.Adapt<IEnumerable<TViewModel>, IEnumerable<TEntity>>(viewModels);
+    }
+  }
+}
---
+++ dotnetcore/dnc/sources/dnc.infra.instance/Instance.cs
@@ -0,0 +1,15 @@
+﻿namespace dnc.infra.instance
+{
+  public static class Instance
+  {
+    public static TClass Get<TClass>() where TClass : class, new() =>
+      new TClass();
+
+    public static void Set<TClass>(out TClass instance) where TClass : class, new() =>
+      instance = Get<TClass>();
+
+    public static bool IsInstance<TClass>(this TClass instance) where TClass : class =>
+      instance != null;
+
+  }
+}
